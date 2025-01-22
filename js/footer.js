ff4802bed70f7679cf6bb4c8b88a8a7b60cb5264
Mountain1016 <mountain1016@outlook.jp>
2025-01-23 05:19:41 +0900

    first commit

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = document.createElement('footer');
+    footer.innerHTML = `
+        <footer>
+            <p>&copy; 2025 まふぃ</p>
+        </footer>
+    `;
+    document.body.appendChild(footer);
+});
